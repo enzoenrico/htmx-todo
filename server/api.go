63a98470cd7669b233ab8ad2482f46674aded7cc
Enzo Enrico <47016293+enzoenrico@users.noreply.github.com>
2024-02-17 23:58:44 +0000

    moving back to main

--- server/api.go
+++ server/api.go
@@ -1,8 +1,8 @@
 package server
 
 import (
-	"fmt"
 	"net/http"
+	"text/template"
 	"time"
 )
 
@@ -12,16 +12,38 @@ type Todos struct {
 	Created     time.Time
 }
 
-var todos []Todos
+	var data map[string][]Todos = map[string][]Todos{
+		"Data":{
+			Todos{
+				Todo: "Lorem",
+				Description: "Ipsum",
+				Created: time.Time{},
+			},
+		},
+	}
 
 // The Serve function sets up a route for the "/" path to call the greet function and starts a server
 // listening on port 8080.
 func Serve() {
 	http.HandleFunc("/", greet)
+	http.HandleFunc("/api/newentry", newentry)
 	http.ListenAndServe(":8080", nil)
 }
 
 
 func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	// fmt.Fprintf(w, "Hello World")
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	tmpl.Execute(w, data)
+}
+
+// The `newentry` function creates a new entry in a list of todos based on the POST request data.
+func newentry(w http.ResponseWriter, r *http.Request) {
+	var res Todos = Todos{
+		Todo: r.PostFormValue("title"),
+		Description: r.PostFormValue("description"),
+		Created: time.Now(),
+	}
+
+	data["Data"] = append(data["Data"], res)
 }
